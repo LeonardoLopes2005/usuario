aa74924824dc0f90d662e8f76f1e21da7414a798
Leonardo Lopes <leonardo.oliveira.lo451@gmail.com>
2025-02-04 15:15:32 -0300

    adicionados etodos get, delete e login

--- src/main/java/com/javanauta/usuario/business/UsuarioService.java
+++ src/main/java/com/javanauta/usuario/business/UsuarioService.java
@@ -3,8 +3,11 @@ package com.javanauta.usuario.business;
 import com.javanauta.usuario.business.converter.UsuarioConverter;
 import com.javanauta.usuario.business.dto.UsuarioDTO;
 import com.javanauta.usuario.infrastructure.entity.Usuario;
+import com.javanauta.usuario.infrastructure.exceptions.ConflitException;
+import com.javanauta.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +16,46 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
+
+    /*a linha de código a seguir ele cria um medoto
+     salvaUsuario, criando um objeto usuário, com isso ele pega dados do dto e passa para
+     o usuário gravar no banco de dados, as primeiro ele tem a exceção de verificar se o email fornecido
+     ja contem no banco de dados, se não conter ele segue para salvar no banco de dados as informações recebida pelo usuário do usuárioDTO
+     e com isso criando u obejto usuário
+    */
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
         usuario = usuarioRepository.save(usuario);
         return usuarioConverter.paraUsuarioDTO(usuario);
+    }
+
+    // Exceção para quando um email existir, puxando um controller
+    public void emailExiste(String email){
+        try{
+            boolean existe = verificaEmailExistente(email);
+            if(existe){
+                throw new ConflitException("Email ja cadastrado" + email);
+            }
+        }catch (ConflitException e){
+            throw new ConflitException("Email ja cadastrado" + e.getCause());
+        }
+    }
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+
+    //busca o usuario por email
+    public Usuario buscarUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Email nao encontrado" + email));
+    }
+
+    // Deleta usuario pro email
+    public void deletaUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
     }
 }
