38422b791109813bbd6461e9a0502677d2c4615f
Leonardo Lopes <leonardo.oliveira.lo451@gmail.com>
2025-02-19 20:20:17 -0300

    adiconados classes exceptions

---
+++ src/main/java/com/javanauta/usuario/controller/GlobalExcaptionHandler.java
@@ -0,0 +1,31 @@
+package com.javanauta.usuario.controller;
+
+
+import com.javanauta.usuario.infrastructure.exceptions.ConflitException;
+import com.javanauta.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import com.javanauta.usuario.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.lang.module.ResolutionException;
+
+@ControllerAdvice
+public class GlobalExcaptionHandler {
+
+    @ExceptionHandler(ResolutionException.class)
+    public ResponseEntity<String> handlerResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflitException.class)
+    public ResponseEntity<String> handleConflictException(ConflitException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.UNAUTHORIZED);
+    }
+}
